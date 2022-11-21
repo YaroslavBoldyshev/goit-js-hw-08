2ebe254b4b566db785a2724501a1df15a2faf27b
YaroslavBoldyshev <110547217+YaroslavBoldyshev@users.noreply.github.com>
2022-11-21 06:03:02 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,26 @@
+import throttle from "lodash.throttle";
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('[name="email"]')
+const message = document.querySelector('[name="message"]')
 
+if (localStorage.length != 0) {
+    if (JSON.parse(localStorage.getItem("feedback-form-state")).emailValue !== "") {
+        email.value = JSON.parse(localStorage.getItem("feedback-form-state")).emailValue;
+    }
+    if (JSON.parse(localStorage.getItem("feedback-form-state")).messageValue !== "") {
+        message.value = JSON.parse(localStorage.getItem("feedback-form-state")).messageValue;
+    }
+}
+form.addEventListener('input', throttle(() => {
+    const emailValue = email.value;
+    const messageValue = message.value;
+    const obj = {emailValue, messageValue}
+    localStorage.setItem("feedback-form-state", JSON.stringify(obj));
+}, 500))
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
+    localStorage.clear();
+    email.value = '';
+    message.value = '';
+})
